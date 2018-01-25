ba217f0cf84535460e7146a58de34f63caeb6503
zxm0420 <zxm0420@users.noreply.github.com>
2018-01-25 15:06:22 +0800

    修改数据库

--- application/publishpro/controller/Publishpro.php
+++ application/publishpro/controller/Publishpro.php
@@ -1,13 +1,19 @@
 <?php
 namespace app\publishpro\controller;
 use think\Controller;
+use think\Db;
 class Publishpro extends Controller
 {
+    //跳转到众筹发布页
     public function jumpToProBaseMsg()
     {
+        //获取项目分类
+        $proSort=Db::table('zc_sort')->select();
+        var_dump($proSort);
+        echo "22";
         return $this->fetch('proBaseMsg');
     }
-
+    //跳转到回报详情页
     public function jumpToAddReturn(){
         return $this->fetch('addReturn');
     }
