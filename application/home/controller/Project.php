f5d6bbc6e5722510b260edc60beb1effcc20b7b8
lintyz <35714999+lintyz@users.noreply.github.com>
2018-01-27 16:49:46 +0800

    排序

--- application/home/controller/Project.php
+++ application/home/controller/Project.php
@@ -18,25 +18,59 @@ class Project extends Controller
     public function proindex(){
         cookie( null,'pro_');//清空前缀为pro_的cookie
         $condition=[];
+        $ord="";
+        $ordertype="";
         $sortid=input('?get.sortid')?input('get.sortid'):"";//分类id
         //$stateid=input('?get.stateid')?input('get.stateid'):"";
         $statename=input('?get.staname')?input('get.staname'):"";//状态
-        $select=input('?get.select')?input('get.select'):"";
-        echo $select;
+        $order=input('?get.order')?input('get.order'):"";
+        $pageParam= ['query' =>[]];//分页条件
+        $pageParam['query']['order'] = $order;
         //获取分类
         $sort=db('sort')->select();
         //按项目分类搜索
         if($sortid!=""){
             cookie('pro_sortid',$sortid,3600);//将查询的分类id存入cookie
             $condition["sortid"]=$sortid;
+            $pageParam['query']['sortid'] = $sortid;
         }
         //按项目状态搜索
         if($statename!=""){
             cookie('pro_staname',$statename,3600);//将查询的项目状态id存入cookie
             $condition["statename"]=$statename;
+            $pageParam['query']['statename'] = $statename;
         }
-        $pro=db('project')->where($condition)->paginate(4);
-        $pronum=db('project')->where($condition)->count('projectid');
+        //排序
+        switch ($order){
+            case "":
+                cookie('order',null);
+                break;
+            case 1://按热度排序
+                cookie('order',$order,3600);
+                $ord='focuscount';
+                $ordertype='acs';
+                break;
+            case 2://最新上线
+                cookie('order',$order,3600);
+                $ord='begintime';
+                $ordertype='desc';
+                $condition['statename']='众筹中';
+                break;
+            case 3://按金额排序
+                cookie('order',$order,3600);
+                $ord='tolamount';
+                $ordertype='desc';
+                break;
+/*            case 4://按支持人数排序
+                cookie('order',$order,3600);
+                $ord='tolamount';
+                $ordertype='desc';
+                break;*/
+        }
+
+        //获取项目（不包括审核中的项目）
+        $pro=db('project')->where($condition)->order($ord,$ordertype)->paginate(4, false, $pageParam);//->where('statename','not in','审核中')
+        $pronum=db('project')->where($condition)->count('projectid');//->where('statename','not in','审核中')
         $this->assign('sortid',cookie('pro_sortid'));//给前端返回搜索的分类id
         $this->assign('sortList',$sort);
         $this->assign('pronum',$pronum);
@@ -50,8 +84,6 @@ class Project extends Controller
         $proid=input('?get.proid')?input('get.proid'):"";
         //获取项目信息
         $pro=db('project')->where('projectid',$proid)->find() ;
-        //项目简介(图片)
-        //$proimg=db('proimg')->where('projectid',$proid)->select() ;
         //项目发起人信息
         //$username=db('user')->where('userid',$pro['userid'])->column('nickname,nickname');
         //$headimg=db('user')->where('userid',$pro['userid'])->column('headimg,headimg');
@@ -60,7 +92,6 @@ class Project extends Controller
         //$this->assign("username",$username[0]);//发起人姓名
        // $this->assign('headimg',$headimg[0]);//发起人头像
         $this->assign("pro",$pro);//项目信息
-        //$this->assign("proimg",$proimg);//项目简介(图片)
         $this->assign("proList",$prodetails);//项目详情
         return $this->fetch();
     }
