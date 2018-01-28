94456888126b9f23e67300043b724408c4c792a4
jzc361 <jzc361@163.com>
2018-01-29 01:00:43 +0800

    个人中心

--- application/home/controller/User.php
+++ application/home/controller/User.php
@@ -14,15 +14,42 @@ class User extends  Controller
     {
         return $this->fetch('userView');
     }
-    //个人设置
+    //支持的项目页面
+    public function support()
+    {
+        return $this->fetch('support');
+    }
+    //我的项目页面
+    public function myProject()
+    {
+        return $this->fetch('myProject');
+    }
+    //关注的项目页面
+    public function focus()
+    {
+        return $this->fetch('focus');
+    }
+    //资金管理页面
+    public function money()
+    {
+        return $this->fetch('money');
+    }
+    //个人设置页面
     public function settings()
     {
         return $this->fetch('settings');
     }
-    //个人设置test
+    //收货地址页面
+    public function address()
+    {
+        return $this->fetch('address');
+    }
+
+
+    //test
     public function test()
     {
-        return $this->fetch('test3');
+        return $this->fetch('money');
     }
     //
 }
