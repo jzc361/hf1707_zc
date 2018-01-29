cab5a66b77689e440076511a943ce951f1a0762f
zxm0420 <zxm0420@users.noreply.github.com>
2018-01-29 23:44:03 +0800

    普通众筹查询

--- application/admin/controller/Promanage.php
+++ application/admin/controller/Promanage.php
@@ -13,26 +13,72 @@ class Promanage extends Controller
         parent::__construct($request);
     }
 
-    //显示全部项目
+    //显示项目列表
     public function index()
     {
-        //获取所有项目
-        if(isset($_GET['sortid'])){
-            echo "1111";
-            //echo input('get.sortid');
-            exit();
+        //显示某分类
+        if(input('?get.sortid') && !input('?get.state')){
+            $selectsortid=input('get.sortid');
+            $allProList=Db::table('zc_project')
+                ->alias('a')
+                ->join('zc_sort b','a.sortid=b.sortid')
+                ->where('a.sortid',$selectsortid)
+                ->order('createtime', 'desc')->paginate(2,false,$config = ['query'=>array('sortid'=>$selectsortid)]);
+                $this->assign('selectsortid',$selectsortid);
         }
-        $allProList=Db::table('zc_project')
-            ->alias('a')
-            ->join('zc_sort b','a.sortid=b.sortid')
-            ->order('createtime', 'desc')->paginate(3);
+        //显示某状态
+        else if(input('?get.state') && !input('?get.sortid')){
+            $state=input('get.state');
+            $allProList=Db::table('zc_project')
+                ->alias('a')
+                ->join('zc_sort b','a.sortid=b.sortid')
+                ->where('a.statename',$state)
+                ->order('createtime', 'desc')->paginate(2,false,$config = ['query'=>array('state'=>$state)]);
+//            $this->assign('selectsortid',$selectsortid);
+        }
+        //显示某分类加某状态
+        else if(input('?get.sortid') && input('?get.state')){
+            $selectsortid=input('get.sortid');
+            $state=input('get.state');
+            $where=[
+                'a.sortid'=>$selectsortid,
+                'a.statename'=>$state,
+            ];
+            $allProList=Db::table('zc_project')
+                ->alias('a')
+                ->join('zc_sort b','a.sortid=b.sortid')
+                ->where($where)
+                ->order('createtime', 'desc')->paginate(2,false,$config = ['query'=>array('sortid'=>$selectsortid)]);
+            $this->assign('selectsortid',$selectsortid);
+        }
+        else{
+            //获取所有项目
+            $allProList=Db::table('zc_project')
+                ->alias('a')
+                ->join('zc_sort b','a.sortid=b.sortid')
+                ->order('createtime', 'desc')->paginate(3);
+        }
+        $this->assign('allProList',$allProList);
         //获取项目分类
         $proSort=Db::table('zc_sort')->select();
-        $this->assign('allProList',$allProList);
         $this->assign('proSort',$proSort);
+        //获取项目状态
+        $result = Db::query('show columns from zc_project like "statename"');
+       // var_dump($result);
+        $enum=$result[0]['Type'];
+        $enum=str_replace("'", '', $enum); //去掉单引号（将单引号改成空）
+        //echo $enum."<br>";
+        $enum_arr=explode("(",$enum);
+        $enum=$enum_arr[1];
+        $enum_arr=explode(")",$enum);
+        $enum=$enum_arr[0];
+        $enum_arr=explode(",",$enum);
+
+        $this->assign('stateList',$enum_arr);
 //        var_dump($allProList);exit();
         return $this->fetch('allProView');
     }
+
     //显示项目详情
     public function showDetails(){
         $projectid=input('get.id');
