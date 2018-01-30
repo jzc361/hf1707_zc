922748f9c8562af1e43208bc3d5611bb8617431a
lintyz <35714999+lintyz@users.noreply.github.com>
2018-01-30 17:33:22 +0800

    评论,关注

--- application/home/controller/Project.php
+++ application/home/controller/Project.php
@@ -20,11 +20,12 @@ class Project extends Controller
     public function proindex(){
         cookie( null,'pro_');//清空前缀为pro_的cookie
         $condition=[];
+        $condition['projecttype']='普通众筹';
         $ord="";
         $ordertype="";
         $sortid=input('?get.sortid')?input('get.sortid'):"";//分类id
-        //$stateid=input('?get.stateid')?input('get.stateid'):"";
-        $statename=input('?get.staname')?input('get.staname'):"";//状态
+        $stateid=input('?get.stateid')?input('get.stateid'):"";//状态id
+        //$statename=input('?get.staname')?input('get.staname'):"";
         $order=input('?get.order')?input('get.order'):"";
         //$search=input('?get.search')?input('get.search'):"";//搜索
         /*if($search!=""){
@@ -35,6 +36,10 @@ class Project extends Controller
         $pageParam['query']['order'] = $order;
         //获取分类
         $sort=db('sort')->select();
+        //获取状态--众筹中，众筹成功，众筹失败
+        $stateType['stateid']=['>',4];
+        $state=db('state')->where($stateType)->select();
+        //var_dump($state);exit;
         //按项目分类搜索
         if($sortid!=""){
             cookie('pro_sortid',$sortid,3600);//将查询的分类id存入cookie
@@ -42,10 +47,11 @@ class Project extends Controller
             $pageParam['query']['sortid'] = $sortid;
         }
         //按项目状态搜索
-        if($statename!=""){
-            cookie('pro_staname',$statename,3600);//将查询的项目状态id存入cookie
-            $condition["statename"]=$statename;
-            $pageParam['query']['statename'] = $statename;
+        if($stateid!=""){
+            cookie('pro_stateid',$stateid,3600);//将查询的项目状态id存入cookie
+            $stateType="";
+            $condition["stateid"]=$stateid;
+            $pageParam['query']['stateid'] = $stateid;
         }
         //条件搜索
         /*if(cookie('search')!=""){
@@ -66,7 +72,6 @@ class Project extends Controller
                 cookie('order',$order,3600);
                 $ord='begintime';
                 $ordertype='desc';
-                $condition['statename']='众筹中';
                 break;
             case 3://按金额排序
                 cookie('order',$order,3600);
@@ -80,29 +85,43 @@ class Project extends Controller
                 break;*/
         }
 
-        //获取项目（不包括审核中的项目）
-        $pro=db('project')->where($condition)->order($ord,$ordertype)->paginate(4, false, $pageParam);//->whereOr('intro','like','%'.$search.'%')->where('statename','not in','审核中')
-        $pronum=db('project')->where($condition)->count('projectid');//->whereOr('intro','like','%'.$search.'%')->where('statename','not in','审核中')
+        //获取项目
+        /*$pro=Db::table('zc_project')
+            ->alias('a')
+            ->join('zc_state b','a.stateid=b.stateid')
+            ->where($condition)
+            ->where($stateType)
+            ->order($ord,$ordertype)
+            ->paginate(4, false, $pageParam);
+        var_dump($pro);exit;*/
+        $pro=db('project')->where($condition)->where($stateType)->order($ord,$ordertype)->paginate(4, false, $pageParam);//->whereOr('intro','like','%'.$search.'%')
+        //var_dump($pro);exit;
+        $pronum=db('project')->where($condition)->where($stateType)->count('projectid');//->whereOr('intro','like','%'.$search.'%')
         $this->assign('sortid',cookie('pro_sortid'));//给前端返回搜索的分类id
-        $this->assign('sortList',$sort);
+        $this->assign('stateid',cookie('pro_stateid'));//给前端返回搜索的状态id
+        $this->assign('sortList',$sort);//分类列表
+        $this->assign('stateList',$state);//状态列表
         $this->assign('pronum',$pronum);
         $this->assign('pro',$pro);
         return $this->fetch();
     }
 
-    //项目详情
+    //项目详情（页面）
     public function prodetails(){
         //项目id
         $proid=input('?get.proid')?input('get.proid'):"";
         //获取项目信息
         $pro=db('project')->where('projectid',$proid)->find() ;
+        //获取项目评论数
+        $commentnum=db('comments')->where(['projectid'=>$proid,'commentto'=>0])->count('projectid');
         //项目发起人信息
-        //$username=db('user')->where('userid',$pro['userid'])->column('nickname,nickname');
-        //$headimg=db('user')->where('userid',$pro['userid'])->column('headimg,headimg');
+        $username=db('user')->where('userid',$pro['userid'])->column('username,username');
+        $headimg=db('user')->where('userid',$pro['userid'])->column('headimg,headimg');
         //项目详情
         $prodetails=db('prodetails')->where('projectid',$proid)->select() ;
-        //$this->assign("username",$username[0]);//发起人姓名
-       // $this->assign('headimg',$headimg[0]);//发起人头像
+        $this->assign("username",$username[0]);//发起人姓名
+        $this->assign('headimg',$headimg[0]);//发起人头像
+        $this->assign('commentnum',$commentnum);//项目评论数
         $this->assign("pro",$pro);//项目信息
         $this->assign("proList",$prodetails);//项目详情
         return $this->fetch();
@@ -112,9 +131,61 @@ class Project extends Controller
         return $this->fetch();
     }*/
 
+    //关注项目
+    public function proFocus(){
+        $proid=input('?post.proid')?input('post.proid'):"";//关注的项目id
+        $userid=session('zc_user')[0]['userid'];//当前登录用户的id
+        $now_time = date('Y-m-d H:i:s',time());//当前时间
+        $condition=[
+            'projectid'=>$proid,
+            'userid'=>$userid
+        ];
+        if(!session('zc_user')){
+            //未登录
+            $reMsg=[
+                'code'=>00000,
+                'msg'=>config('Msg')['nologin']['nologin'],
+                'data'=>[]
+            ];
+            return json($reMsg);
+        }
+        //查看关注表，用户是否关注过该项目
+        $res=db('focuspro')->where($condition)->select();
+        if(!empty($res)){
+            //已关注
+            $reMsg=[
+                'code'=>40003,
+                'msg'=>config('Msg')['focus']['already'],
+                'data'=>[]
+            ];
+            return json($reMsg);
+        }else{
+            $condition['focustime']=$now_time;
+            $res=db('focuspro')->insert($condition);
+            if($res){
+                //关注成功
+                $reMsg=[
+                    'code'=>40001,
+                    'msg'=>config('Msg')['focus']['success'],
+                    'data'=>[]
+                ];
+                return json($reMsg);
+            }else{
+                //关注失败
+                $reMsg=[
+                    'code'=>40002,
+                    'msg'=>config('Msg')['focus']['error'],
+                    'data'=>[]
+                ];
+                return json($reMsg);
+            }
+        }
+    }
+
     //项目评论（页面）
-    public function prodetails_comment(){
-        $proid=input('?get.proid')?input('get.proid'):"";
+    public function showComment(){
+        $proid=input('?get.proid')?input('get.proid'):"";//项目id
+        $pageParam['query']['proid'] = $proid;
         //$data=db('comments')->where('projectid',$proid)->select();
         //多表查询
         $data=Db::table('zc_comments')
@@ -124,19 +195,32 @@ class Project extends Controller
             ->order('a.ctime','desc')
             ->field('a.*,b.userid,b.username,b.headimg')
             ->select();
+            //->paginate(4,false,$pageParam);
         $this->assign('comments',$data);
         $this->assign('proid',$proid);
-        return $this->fetch();
+        return $this->fetch('prodetails_comment');
     }
 
     //评论
-    public function comment(){
-        $proid=input('?post.proid')?input('post.proid'):"";
-        $comment=input('?post.comment')?input('post.comment'):"";
-        $userid=session('zc_user')[0]['userid'];
+    public function comToPro(){
+        $proid=input('?post.proid')?input('post.proid'):"";//被评论的项目id
+        $content=input('?post.content')?input('post.content'):"";//评论内容
+        $commentto=input('?post.commentto')?input('post.commentto'):0;//回复/评论->0
+        $userid=session('zc_user')[0]['userid'];//发表评论的用户的id
+        $now_time = date('Y-m-d H:i:s',time());//当前时间
         $condition=['projectid'=>$proid,'userid'=>$userid];
+        //未登录
+        if(!session('zc_user')){
+            //未登录
+            $reMsg=[
+                'code'=>00000,
+                'msg'=>config('Msg')['nologin']['nologin'],
+                'data'=>[]
+            ];
+            return json($reMsg);
+        }
         //评论内容为空
-        if(!$comment){
+        if(!$content){
             $reMsg=[
                 'code'=>30004,
                 'msg'=>config('Msg')['comment']['null'],
@@ -145,8 +229,11 @@ class Project extends Controller
             return json($reMsg);
         }
         //查看用户订单表，用户是否支持过该项目
-        $res=db('orders')->where($condition)->select();
-        if(empty($res)){
+        $res1=db('orders')->where($condition)->select();
+        //判断当前用户是否为项目发起人
+        $res2=db('project')->where($condition)->count('projectid');
+        //var_dump($res2);exit;
+        if(empty($res1)&&$res2==0){
             //未支持项目
             $reMsg=[
                 'code'=>30003,
@@ -155,12 +242,32 @@ class Project extends Controller
             ];
             return json($reMsg);
         }else{
-            $reMsg=[
-                'code'=>30003,
-                'msg'=>config('Msg')['comment']['success'],
-                'data'=>[]
+            $condition=[
+                'projectid'=>$proid,
+                'userid'=>$userid,
+                'ctime'=>$now_time,
+                'content'=>$content,
+                'commentto'=>$commentto
             ];
-            return json($reMsg);
+            //将评论内容插入数据库
+            $res=db('comments')->insert($condition);
+            if($res){
+                //评论成功
+                $reMsg=[
+                    'code'=>30001,
+                    'msg'=>config('Msg')['comment']['success'],
+                    'data'=>[]
+                ];
+                return json($reMsg);
+            }else{
+                //评论失败
+                $reMsg=[
+                    'code'=>30002,
+                    'msg'=>config('Msg')['comment']['commentFail'],
+                    'data'=>[]
+                ];
+                return json($reMsg);
+            }
         }
     }
 }
