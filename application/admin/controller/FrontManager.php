3a5a6246998deff9acf7a42fbb533070e4539b56
Hdyang97 <1208782907@qq.com>
2018-02-24 17:12:18 +0800

    广告修改

--- application/admin/controller/FrontManager.php
+++ application/admin/controller/FrontManager.php
@@ -8,7 +8,38 @@ class FrontManager extends Controller
 {
 
     public function toIframe($htmlName){
+        $adList=db('ad')
+            ->select();
+        foreach($adList as &$val){
+            if(strtotime($val['endtime'])<time()){//结束时间小于当前时间
+                $state = '已过期';
+            }else if(strtotime($val['starttime'])>time()){//开始时间大于当前时间
+                $state = '未上架广告';
+            }else{
+                $state = '使用中';
+            }
+            $val['state'] = $state;
+        }
+        $this->assign('adList',$adList);
         return $this->fetch($htmlName);
     }
+    public function adAdd(){
 
+    }
+    public function adEditData(){
+        $editId = isset($_POST['editId'])?$_POST['editId']:"";
+        $adList=db('ad')
+            ->where('adid',$editId)
+            ->select(); //获取有效期广告
+        $res = [
+          'code'=>1001,
+            'msg'=>'查询成功',
+            'data'=>$adList
+        ];
+        return $res;
+    }
+
+    public function adDelete(){
+
+    }
 }
